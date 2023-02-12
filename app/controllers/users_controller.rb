a9b304c5d35d44d50f1a90bad47d97ff8f6ac28e
willsampson <wsampson@uchicago.edu>
2023-02-12 17:23:31 +0000

    Final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
 
     matching_users = User.all
@@ -13,5 +14,33 @@ class UsersController < ApplicationController
     @the_user = matching_users.at(0)
     render({ :template => "user_templates/show_details.html.erb"})
   end 
+
+  def add_user
+    input_username = params.fetch("input_username")
+
+    new_user = User.new
+    new_user.username = input_username
+    new_user.save
+
+    redirect_to( "/users/" + input_username)
+  end
+
+  def update
+    the_id = params.fetch("modify_id")
+    matching_user = User.where({ :id => the_id})
+    the_user_update = matching_user.first
+
+    updated_user = params.fetch("new_user")
+
+    the_user_update.username = updated_user
+
+    the_user_update.save
+
+    redirect_to( "/users/" + the_user_update.username )
+  end
+
+  def home
+    redirect_to( "/users")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "users", :action => "home"})
 
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:user_id", { :controller => "users", :action => "details"})
+  get("/add_user", { :controller => "users", :action => "add_user"})
+  get("/update_user/:modify_id", { :controller => "users", :action => "update"})
 
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:photo_id", { :controller => "photos", :action => "details"})
-  
   get("/delete_photo/:photo_id", { :controller => "photos", :action => "destroy"})
   get("/insert_photo", { :controller => "photos", :action => "create"})
   get("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
